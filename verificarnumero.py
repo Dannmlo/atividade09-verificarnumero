37299ae1f9babf7f2cfe0fdf684649d962323f90
Dannmlo <antonio.melo114@aluno.ce.gov.br>
2023-10-23 13:26:00 -0300

    Verificação de número

---
+++ verificarnumero.py
@@ -0,0 +1,6 @@
+#Exercício Python 09: faça um software que verifique entre 2 numeros qual o maior
+n1 =int(input("Digite o primeiro numero:"))
+n2 =int(input("Digite o segundo numero:"))
+if (n1>=n2):
+    print (f"O maior numero é: {n1}")
+else : print(f"O maior numero é: {n2}")
